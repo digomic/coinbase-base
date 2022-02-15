2159befa81c764c9f730b277ac41af01db7b98d5
diego <digomic@outlook.es>
2022-02-15 07:01:20 -0600

    add pages

---
+++ pages/Dashboard.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
+import styled from 'styled-components';
+import Main from '../components/Main';
+import Sidebar from '../components/Sidebar';
+import { ethers } from "ethers"
+import { ThirdwebSDK } from '@3rdweb/sdk';
+import Head from 'next/head';
+
+const sdk = new ThirdwebSDK(
+  new ethers.Wallet(
+    process.env.NEXT_PUBLIC_METAMASK_KEY,
+    ethers.getDefaultProvider(
+      'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
+    )
+  )
+)
+
+const Dashboard = ({address}) => {
+  const [sanityTokens, setSanityTokens] = useState([]);
+  const [thirdWebTokens, setThirdWebTokens] = useState([]);
+
+  {/* call the api*/}
+  useEffect(() => {
+    const getSanityAndThirdWebTokens = async () => {
+      const coins = await fetch(
+        "https://k3gnbo2h.api.sanity.io/v1/data/query/production?query=*%5B_type%3D%3D%27coins%27%5D%20%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D"
+      )
+      const sanityTokens = (await coins.json()).result;
+      setSanityTokens(sanityTokens);
+
+      setThirdWebTokens(
+        sanityTokens.map(token => sdk.getTokenModule(token.contractAddress))
+      )
+    }
+
+    return getSanityAndThirdWebTokens();
+  }, [])
+  //console.log('sanity ', sanityTokens)
+  //console.log('Thridweb', thirdWebTokens)
+
+
+  return (
+    <>
+    <Head>
+      <title>Dashboard</title>
+    </Head>
+    <Wrapper>
+      <Sidebar />
+      <MainContainer>
+        <Header
+          walletAddress={address}
+          sanityTokens={sanityTokens}
+          thirdWebTokens={thirdWebTokens}
+        />
+        <Main
+          walletAddress={address}
+          sanityTokens={sanityTokens}
+          thirdWebTokens={thirdWebTokens}
+        />
+      </MainContainer>
+    </Wrapper>
+  </>
+  )  
+}
+
+export default Dashboard;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+  overflow: hidden;
+`
+
+const MainContainer = styled.div`
+  flex: 1;
+`
